6755104523dbdaaacf0244f8e7f952e1236b7968
JMSCE <44677923+JMSCE@users.noreply.github.com>
2018-12-17 22:39:09 +0000

    Update Guessgame.py

--- Guessgame.py
+++ Guessgame.py
@@ -1,19 +1,15 @@
 import random
-magic_word = ("cat")
+magic_word = ("flamingo")
 guess_count = 0
-guess_limit = 3
+guess_limit = 4
 out_of_guesses = False
-def hint_help():
-  hints = ["It has four legs", "It says 'meow'", "It's likes milk"]
-  random.shuffle(hints)
-  for hint_function in hints[0,2]:
-      return result
-print("""Welcome to the guessing game. 
-You will have three attempts to guess the magic word.""")
+hints = ["They aren't tall but they can be up to 5ft wide.", "Think, red + white = what?", "When they eat, their head is positioned upside-down", "It isn't a dog", "You wont find them in the old world.", "Don't ask one to dance, it wants to stand on one leg."]
+print("""Welcome to the guessing game. You will have five attempts to guess the magic word.""")
 guess = input("What is your guess?:")
 while guess != magic_word and not (out_of_guesses):
     if guess_count < guess_limit:
-        hint_help()
+        hint = random.choice(hints) 
+        print("Hint: "+ hint)
         guess = input("Enter a guess: ")
         guess_count += 1
     else:
