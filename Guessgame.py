4db4ca03c8b9a42cab7936c131b8d90091c564f4
JMSCE <44677923+JMSCE@users.noreply.github.com>
2018-12-17 23:01:28 +0000

    Update Guessgame.py

--- Guessgame.py
+++ Guessgame.py
@@ -3,8 +3,9 @@ magic_word = ("flamingo")
 guess_count = 0
 guess_limit = 4
 out_of_guesses = False
-hints = ["They aren't tall but they can be up to 5ft wide.", "Think, red + white = what?", "When they eat, their head is positioned upside-down", "It isn't a dog", "You wont find them in the old world.", "Don't ask one to dance, it wants to stand on one leg."]
-print("""Welcome to the guessing game. You will have five attempts to guess the magic word.""")
+hints = ("They aren't tall but they can be up to 5ft wide.", "Think, red + white = what?", "When they eat, their head is positioned upside-down", "It isn't a dog", "You wont find them in the old world.", "Don't ask one to dance, it wants to stand on one leg.")
+name = input("Enter your name: ")
+print("Hi " + name + " that is a really stupid name. You must be embarrassed. Anyway, welcome to the guessing game. You will have five attempts to guess the magic word.")
 guess = input("What is your guess?:")
 while guess != magic_word and not (out_of_guesses):
     if guess_count < guess_limit:
@@ -15,6 +16,6 @@ while guess != magic_word and not (out_of_guesses):
     else:
         out_of_guesses = True
 if out_of_guesses:
-    print("You lose!")
+    print("You lose! " + name + " That is a real shame. Boo hoo.")
 else:
     print("You win!")
