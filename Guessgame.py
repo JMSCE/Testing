5b1871cbed441e480d687ec7cadde1493e0c105a
JMSCE <44677923+JMSCE@users.noreply.github.com>
2018-12-18 13:35:38 +0000

    Update Guessgame.py

--- Guessgame.py
+++ Guessgame.py
@@ -1,12 +1,12 @@
 import random
-magic_word = ("flamingo")
+magic_word = ("flamingo") 
 guess_count = 0
 guess_limit = 4
 out_of_guesses = False
 hints = ["They aren't tall but they can be up to 5ft wide.", "Think, red + white = what?", "When they eat, their head is positioned upside-down", "It isn't a dog", "You wont find them in the old world.", "Don't ask one to dance, it wants to stand on one leg."]
 name = input("Enter your name: ")
 print("Hi " + name + " that is a really stupid name. You must be embarrassed. Anyway, welcome to the guessing game. You will have five attempts to guess the magic word.")
-guess = input("What is your guess?:")
+guess = str.lower(input("What is your guess?:"))
 while guess != magic_word and not (out_of_guesses):
     if guess_count < guess_limit:
         random.shuffle(hints)
